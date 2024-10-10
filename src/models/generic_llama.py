621b597d8cbe887920ba28401707cb0d54efc503
Catalin Mihit <catalin.mihit2002@gmail.com>
2024-10-10 13:09:02 +0200

    Added outline for models

---
+++ src/models/generic_llama.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+from typing import Any, Union
+import os
+
+
+class GenericLlama(ABC):
+	""" Interface for generic Llama7B model with full finetuning """
+	
+	@abstractmethod
+	def __init__(
+		self,
+		**kwargs
+	):
+		"""
+		Initialize the base Llama7B model
+		"""
+		super().__init__()
+		
+		# self.model = Load Llama7B here
+	
+	
+	@abstractmethod
+	def finetune(
+		self,
+		train_in,
+		train_out,
+		**kwargs
+	) -> Any:
+		"""
+		Function handling the finetuning procedure of the LLM
+		on the given training data pairs in (train_in, train_out)
+		"""
+		pass
+	
+	
+	@abstractmethod
+	def prompt(
+		self,
+		prompt: str,
+		**kwargs
+	) -> Any:
+		"""
+		Function handling the generation of output
+		"""
+		pass
+	
+	
+	@abstractmethod
+	def save(
+		self,
+		path: Union[str, os.path],
+		**kwargs
+	) -> None:
+		"""
+		Save pickled model at the specified path
+		"""
+		# pickle(self.model)
+		pass
+	
+	
+	@abstractmethod
+	def load(
+		self,
+		path: Union[str, os.path],
+		**kwargs
+	) -> None:
+		"""
+		Save pickled model at the specified path
+		"""
+		# self.model = pickle.(load from path)
+		pass
